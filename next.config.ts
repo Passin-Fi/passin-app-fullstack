d991275c5786221e81a14a069e6e6ba9188789a7
Le Dinh Quyen <quyenld9699@gmail.com>
2025-09-15 17:06:52 +0700

    add cicd

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
     output: 'standalone',
+    reactStrictMode: false,
     eslint: { ignoreDuringBuilds: true },
     images: {
         remotePatterns: [{ hostname: 'firebasestorage.googleapis.com', protocol: 'https' }],
