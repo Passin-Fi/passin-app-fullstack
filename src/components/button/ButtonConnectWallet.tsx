da9f471ff51684d54e03d0bb2659cdcddfbf55e1
Le Dinh Quyen <quyenld9699@gmail.com>
2025-09-15 17:40:15 +0700

    fix cicd

--- src/components/button/ButtonConnectWallet.tsx
+++ src/components/button/ButtonConnectWallet.tsx
@@ -1,15 +1,25 @@
 'use client';
-import { useWallet } from '@lazorkit/wallet';
+import { PasskeyData, useWallet } from '@lazorkit/wallet';
 import React from 'react';
 import { Button } from 'shadcn/button';
 
 export default function ButtonConnectWallet() {
     const { createPasskeyOnly, isConnecting, isLoading, smartWalletPubkey, wallet } = useWallet();
+    const [data, setData] = React.useState<PasskeyData | null>(null);
+    async function getPasskeysSelect() {
+        try {
+            const data = await createPasskeyOnly();
+            console.log('data', data);
+            setData(data);
+        } catch (error) {
+            console.error('Error creating passkey-only wallet:', error);
+        }
+    }
 
     return (
         <>
-            <Button onClick={() => console.log({ wallet, smartWalletPubkey })}>Logs</Button>
-            <Button onClick={createPasskeyOnly}>Signup</Button>
+            <Button onClick={() => console.log({ data, wallet, smartWalletPubkey })}>Logs</Button>
+            <Button onClick={getPasskeysSelect}>Signup</Button>
         </>
     );
 }
