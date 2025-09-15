7c906a709f49e98637b8147b67a135d69f84bda9
Le Dinh Quyen <quyenld9699@gmail.com>
2025-09-15 17:57:54 +0700

    test create smartwallet

--- src/components/button/ButtonConnectWallet.tsx
+++ src/components/button/ButtonConnectWallet.tsx
@@ -1,11 +1,12 @@
 'use client';
-import { PasskeyData, useWallet } from '@lazorkit/wallet';
+import { PasskeyData, SmartWalletCreationResult, useWallet } from '@lazorkit/wallet';
 import React from 'react';
 import { Button } from 'shadcn/button';
 
 export default function ButtonConnectWallet() {
-    const { createPasskeyOnly, isConnecting, isLoading, smartWalletPubkey, wallet } = useWallet();
+    const { createPasskeyOnly, isConnecting, isLoading, smartWalletPubkey, wallet, buildSmartWalletTransaction, createSmartWalletOnly } = useWallet();
     const [data, setData] = React.useState<PasskeyData | null>(null);
+    const [dataCreateSmartWallet, setDataCreateSmartWallet] = React.useState<SmartWalletCreationResult | null>(null);
     async function getPasskeysSelect() {
         try {
             const data = await createPasskeyOnly();
@@ -15,11 +16,26 @@ export default function ButtonConnectWallet() {
             console.error('Error creating passkey-only wallet:', error);
         }
     }
+    async function createSmartWallet() {
+        try {
+            if (!data) return;
+            const res = await createSmartWalletOnly(data);
+            console.log('dataCreateSmartWallet', res);
+            setDataCreateSmartWallet(res);
+        } catch (error) {
+            console.error('Error creating smart wallet:', error);
+        }
+    }
 
     return (
         <>
-            <Button onClick={() => console.log({ data, wallet, smartWalletPubkey })}>Logs</Button>
-            <Button onClick={getPasskeysSelect}>Signup</Button>
+            <Button onClick={() => console.log({ data, dataCreateSmartWallet, wallet, smartWalletPubkey })}>Logs</Button> <br />
+            <Button onClick={getPasskeysSelect}>Signup</Button> <br />
+            <Button disabled={!data} onClick={createSmartWallet}>
+                {isLoading ? 'Creating...' : 'Create Smart Wallet'}
+            </Button>{' '}
+            <br />
+            {smartWalletPubkey && <div>Smart Wallet: {smartWalletPubkey.toBase58()}</div>}
         </>
     );
 }
